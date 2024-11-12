0631e85250bf4323ffdb877d794d300abb173de4
bhavishyaiitk <106675666+bhavishyaiitk@users.noreply.github.com>
2024-11-12 18:34:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const formattedData = data.map(row => ({
 
 // Serve the main HTML page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Search for a course by name
